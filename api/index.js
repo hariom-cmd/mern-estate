d3e9ad58645ab44b314f8ab4750fa5580e539ae2
hariom-saini <hariomsaini9896@gmail.com>
2024-08-07 01:30:19 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -3,12 +3,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-mongoose
-.connect(process.env.MONGO)
+mongoose.connect(process.env.MONGO)
 .then( () =>{
     console.log('Connected to MongoDB!');
-})
-.catch((err) => {
+}).catch((err) => {
     console.log(err);
 });
 
