166153b242adcd323774693ecbae5d0c2d2db418
hariom-saini <hariomsaini9896@gmail.com>
2024-08-08 00:52:49 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/users.routes.js';
+import authRouter from './routes/auth.routes.js';
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO)
@@ -12,6 +14,10 @@ mongoose.connect(process.env.MONGO)
 });
 
 const app = express();
+
+
+// this will alow json as ipnput at the server
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
@@ -25,3 +31,4 @@ app.listen(3000, () => {
 // here req -> made by the browser by the user AND res -> response is send by the server to the client
 // this api route is related to the api we want to go that specific location(we create a proxy later)
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
